62e2a47cefb41635fc3ddcaf7aee5e0f1ef41e89
Pierre <pierre.de.vreyer@gmail.com>
2019-05-09 23:17:44 +0800

    Added major to student profile

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -40,6 +40,6 @@ class StudentsController < ApplicationController
   end
 
   def student_params
-    params.require(:student).permit(:name, :email)
+    params.require(:student).permit(:name, :email, :major)
   end
 end
---
+++ db/migrate/20190509142242_add_column_major.rb
@@ -0,0 +1,5 @@
+class AddColumnMajor < ActiveRecord::Migration[5.2]
+  def change
+    add_column :students, :major, :string
+  end
+end
